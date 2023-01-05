c65a30393704a61e69248eac60852e611d9fbd68
moneyandfame7 <davidoo1234e@gmail.com>
2023-01-05 23:03:48 +0200

    feat: some changes for deploy was added

--- src/models/index.ts
+++ src/models/index.ts
@@ -8,6 +8,9 @@ const env: string = process.env.NODE_ENV || 'development';
 
 const config: Options = configDB[env as keyof typeof configDB];
 
-const db: Sequelize = new Sequelize(config.database!, config.username!, config.password, config);
+const db: Sequelize = new Sequelize(config.database!, config.username!, config.password, {
+  ...config,
+  dialectModule: pg,
+});
 
 export default db;
