d1672da523219b6ed4bf312889eea676584d6db7
moneyandfame7 <davidoo1234e@gmail.com>
2023-01-05 22:51:15 +0200

    feat: pg import was added

--- dist/src/models/index.js
+++ dist/src/models/index.js
@@ -1,6 +1,10 @@
 import { Sequelize } from 'sequelize';
+import * as pg from 'pg';
 import configDB from './../config/config.js';
 const env = process.env.NODE_ENV || 'development';
 const config = configDB[env];
-const db = new Sequelize(config.database, config.username, config.password, config);
+const db = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialectModule: pg,
+});
 export default db;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,15 @@
 import { Sequelize } from 'sequelize';
 import { Options } from 'sequelize';
-
+import * as pg from 'pg';
 import configDB from './../config/config.js';
 
 const env: string = process.env.NODE_ENV || 'development';
 
 const config: Options = configDB[env as keyof typeof configDB];
 
-const db: Sequelize = new Sequelize(config.database!, config.username!, config.password, config);
+const db: Sequelize = new Sequelize(config.database!, config.username!, config.password, {
+  ...config,
+  dialectModule: pg,
+});
 
 export default db;
