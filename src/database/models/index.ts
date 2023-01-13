404f1b150f0289fa179a57671415490433125f1f
moneyandfame7 <davidoo1234e@gmail.com>
2023-01-13 21:15:09 +0200

    fix

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -6,6 +6,9 @@ const env: string = process.env.NODE_ENV || 'development';
 
 const config: Options = configDB[env as keyof typeof configDB];
 
-const db: Sequelize = new Sequelize(config.database!, config.username!, config.password, config);
+const db: Sequelize = new Sequelize(
+  'postgresql://postgres:WeOfaeYfp6AfcGmILvV8@containers-us-west-76.railway.app:6954/railway',
+  config
+);
 
 export default db;
