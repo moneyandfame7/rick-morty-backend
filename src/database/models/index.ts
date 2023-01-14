6b2df62d9ef74c841eafacf97876450a8e4fce8c
moneyandfame7 <davidoo1234e@gmail.com>
2023-01-14 13:01:50 +0200

    feat: was changed connection method for sequelize

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -3,9 +3,19 @@ import { Options } from 'sequelize';
 import configDB from '../config/config';
 
 const env: string = process.env.NODE_ENV || 'development';
-
 const config: Options = configDB[env as keyof typeof configDB];
+let db: Sequelize;
 
-const db: Sequelize = new Sequelize(config.database!, config.username!, config.password, config);
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true, //false
+  });
+} else {
+  // the application is executed on the local machine
+  db = new Sequelize(config.database!, config.username!, config.password, config);
+}
 
 export default db;
